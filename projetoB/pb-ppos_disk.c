140bd72e50acbb1be06006c71b4d4e8590fafd95
ratatusznei <fernandoratusznei@hotmail.com>
2024-06-26 18:46:25 -0300

    envio projeto B

---
+++ projetoB/pb-ppos_disk.c
@@ -0,0 +1,176 @@
+/*
+ * Equipe:
+ * Fernando Henrique Ratusznei Caetano
+ * Gustavo Esmanhotto Bareta
+ */
+
+#include "ppos_disk.h"
+
+#include "ppos.h"
+#include "disk.h"
+
+disk_t disk_mgr;
+
+pedido_t *cscan(disk_queue_t *q) {
+	pedido_t *next_pedido = NULL;
+	pedido_t *p = q;
+
+	// Verica os acima
+	do {
+		if (p->block >= disk_mgr.curr_block) {
+			if (next_pedido == NULL) {
+				next_pedido = p;
+			}
+			else if (p->block < next_pedido->block) {
+				next_pedido = p;
+			}
+		}
+
+		p = p->next;
+	} while (p != q);
+
+	// Acha o de menor block
+	if (next_pedido == NULL) {
+		disk_mgr.blocks_percorridos += abs(disk_mgr.numBlocks - disk_mgr.curr_block); // move para o final do disco
+		disk_mgr.blocks_percorridos += disk_mgr.numBlocks; // move para o inicio
+		disk_mgr.curr_block = 0;
+
+		next_pedido = q;
+		p = q->next;
+
+		while (p != q) {
+			if (next_pedido == NULL) {
+				next_pedido = p;
+			}
+			else if (p->block < next_pedido->block) {
+				next_pedido = p;
+			}
+
+			p = p->next;
+		}
+	}
+
+	return next_pedido;
+}
+
+pedido_t *sstf(disk_queue_t *q) {
+	int shortest_distance = abs(q->block - disk_mgr.curr_block);
+	pedido_t *shortest_pedido = q;
+	pedido_t *p = q->next;
+
+	while (p != q) {
+		int dist = abs(p->block - disk_mgr.curr_block);
+		if (dist < shortest_distance) {
+			shortest_distance = dist;
+			shortest_pedido = p;
+		}
+
+		p = p->next;
+	}
+
+	return shortest_pedido;
+}
+
+pedido_t *fcfs(disk_queue_t *q) {
+	return q;
+}
+
+void disk_mgr_Body() {
+	while (countTasks > 1) {
+		int result = disk_cmd(DISK_CMD_STATUS, 0, 0);
+		if (result == DISK_STATUS_IDLE && disk_mgr.diskQueue != NULL) {
+			// cscan   4223   26350 ms
+			// sstf    4217   26466 ms
+			// fcfs    7681   30025 ms
+			mutex_lock(&disk_mgr.queue_mutex);
+			pedido_t *p = fcfs(disk_mgr.diskQueue);
+			mutex_unlock(&disk_mgr.queue_mutex);
+	
+			disk_cmd(p->type, p->block, p->buffer) ;
+
+			task_suspend(taskExec, NULL);
+			task_yield();
+
+			disk_mgr.blocks_percorridos += abs(p->block - disk_mgr.curr_block);
+			disk_mgr.curr_block = p->block;
+			
+			task_resume(p->task);
+		}
+
+		task_yield();
+	}
+
+	printf("DISC TASK EXIT\n    blocos percorridos: %d\n", disk_mgr.blocks_percorridos);
+	task_exit(0);
+}
+
+// inicializacao do gerente de disco
+// retorna -1 em erro ou 0 em sucesso
+// numBlocks: tamanho do disco, em blocos
+// blockSize: tamanho de cada bloco do disco, em bytes
+int disk_mgr_init (int *numBlocks, int *blockSize) {
+	if (disk_cmd(DISK_CMD_INIT, 0, 0) < 0) {
+		return -1;
+	}
+
+	*numBlocks = disk_cmd(DISK_CMD_DISKSIZE, 0, 0);
+	if (*numBlocks < 0) {
+		return -1;
+	}
+	disk_mgr.numBlocks = *numBlocks;
+
+	*blockSize = disk_cmd (DISK_CMD_BLOCKSIZE, 0, 0);
+	if (*blockSize < 0) {
+		return -1;
+	}
+
+	task_create(&disk_mgr.taskDisk, disk_mgr_Body, NULL);
+	disk_mgr.taskDisk.user_task = 0;
+	disk_mgr.taskDisk.remaining_time = 999999;
+
+	disk_mgr.curr_block = 0;
+	disk_mgr.blocks_percorridos = 0;
+
+	mutex_create(&disk_mgr.queue_mutex);
+
+	return 0;
+}
+
+int disk_block_command(int cmd, int block, void *buffer) {
+	pedido_t *p = (pedido_t *) malloc(sizeof(pedido_t));
+	p->next = NULL;
+	p->prev = NULL;
+	p->task = taskExec;
+	p->type = cmd;
+	p->block = block;
+	p->buffer = buffer;
+
+	mutex_lock(&disk_mgr.queue_mutex);
+	queue_append((queue_t**) &disk_mgr.diskQueue, (queue_t*) p);
+	mutex_unlock(&disk_mgr.queue_mutex);
+
+	int result = disk_cmd(DISK_CMD_STATUS, 0, 0);
+	if (result == DISK_STATUS_IDLE) {
+		task_resume(&disk_mgr.taskDisk);
+	}
+
+	task_suspend(taskExec, NULL);
+	task_yield();
+
+	mutex_lock(&disk_mgr.queue_mutex);
+	queue_remove((queue_t**) &disk_mgr.diskQueue, (queue_t*) p);
+	mutex_unlock(&disk_mgr.queue_mutex);
+	free(p);
+
+	return 0;
+}
+
+// leitura de um bloco, do disco para o buffer
+int disk_block_read (int block, void *buffer) {
+	return disk_block_command(DISK_CMD_READ, block, buffer);
+}
+
+// escrita de um bloco, do buffer para o disco
+int disk_block_write (int block, void *buffer) {
+	return disk_block_command(DISK_CMD_WRITE, block, buffer);
+}
---
+++ projetoB/pb-ppos_disk.h
@@ -0,0 +1,62 @@
+/*
+ * Equipe:
+ * Fernando Henrique Ratusznei Caetano
+ * Gustavo Esmanhotto Bareta
+ */
+
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DINF UFPR
+// Versão 1.2 -- Julho de 2017
+
+// interface do gerente de disco rígido (block device driver)
+
+#ifndef __DISK_MGR__
+#define __DISK_MGR__
+
+#include "ppos_data.h"
+#include "ppos-core-globals.h"
+
+// estruturas de dados e rotinas de inicializacao e acesso
+// a um dispositivo de entrada/saida orientado a blocos,
+// tipicamente um disco rigido.
+
+// estrutura que representa um disco no sistema operacional
+
+typedef struct disk_queue_t {
+	struct disk_queue_t *next, *prev;
+	struct task_t *task;
+	int type; // READ / WRITE
+	int block;
+	void *buffer;
+} disk_queue_t;
+
+typedef struct disk_queue_t pedido_t;
+
+typedef struct
+{
+	// completar com os campos necessarios
+	int numBlocks;
+	int curr_block;
+	int blocks_percorridos;
+
+	struct task_t taskDisk;
+
+	mutex_t queue_mutex;
+	struct disk_queue_t *diskQueue;
+} disk_t ;
+
+extern disk_t disk_mgr;
+
+// inicializacao do gerente de disco
+// retorna -1 em erro ou 0 em sucesso
+// numBlocks: tamanho do disco, em blocos
+// blockSize: tamanho de cada bloco do disco, em bytes
+int disk_mgr_init (int *numBlocks, int *blockSize) ;
+
+// leitura de um bloco, do disco para o buffer
+int disk_block_read (int block, void *buffer) ;
+
+// escrita de um bloco, do buffer para o disco
+int disk_block_write (int block, void *buffer) ;
+
+#endif
